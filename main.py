2867e6f95d400c5030d22fc47315e9cba20dcb1a
Haerin Kim <lini1634@naver.com>
2024-05-06 02:52:52 +0900

    csrf proect 추가

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify
 import requests
 from bs4 import BeautifulSoup
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
 
 @app.route('/test', methods=['GET'])
 def test():
