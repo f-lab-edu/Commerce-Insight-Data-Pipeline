053d702125243de17de490fd2ab6d39889a4a9d6
Haerin Kim <lini1634@naver.com>
2024-05-06 03:10:19 +0900

    디버그 피쳐 초기 비활성화

--- main.py
+++ main.py
@@ -4,11 +4,15 @@ from bs4 import BeautifulSoup
 from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
+app.debug = False
+app.run(debug=False)
+
 csrf = CSRFProtect()
 csrf.init_app(app) # Compliant
 
 @app.route('/test', methods=['GET'])
 def test():
     return "hi"
+
 if __name__ == '__main__':
     app.run(debug=True)
