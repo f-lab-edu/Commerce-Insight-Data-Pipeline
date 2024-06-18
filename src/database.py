9fbe04827bfff61f72c2903940e9b7e469a5bdef
Haerin Kim <lini1634@naver.com>
2024-06-18 23:28:49 +0900

    코드정리9

--- src/database.py
+++ src/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-local_path = "sqlite:///../tweet_info.db"
+local_path = "sqlite:///./tweet_info.db"
 engine = create_engine(local_path, echo=True)
 Base = declarative_base()
 Base.metadata.create_all(engine)
